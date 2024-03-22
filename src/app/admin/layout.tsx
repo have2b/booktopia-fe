90cad4bfb7b1ecdf4876d2e78e31abaad5b39c4a
hopdinhp02 <hopdinhp02@gmail.com>
2024-03-22 23:32:52 +0700

    fix: cannot apply tailwindcss

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,22 +1,6 @@
 /** @format */
 
-import type { Metadata } from "next";
-import { EB_Garamond, Inter } from "next/font/google";
-import "@/app/globals.css";
-import { cn } from "@/lib/utils";
 import SideNavbar from "@/components/admin/side-navbar";
-
-const inter = Inter({ subsets: ["latin"] });
-
-const font = EB_Garamond({
-  weight: ["400", "500", "600", "700", "800"],
-  subsets: ["latin", "vietnamese"],
-});
-
-export const metadata: Metadata = {
-  title: "Booktopia - Online book store",
-  description: "Developed by LongVT and HopDP",
-};
 
 export default function AdminLayout({
   children,
