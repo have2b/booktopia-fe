3213fc65527feaef445e10c97f48cc94c9db924e
hopdinhp02 <hopdinhp02@gmail.com>
2024-03-22 18:42:09 +0700

    refactor: remove unnecessary comment

--- src/actions/auth/register.ts
+++ src/actions/auth/register.ts
@@ -3,13 +3,7 @@
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
 import axios, { AxiosError } from "axios";
-// : Promise<{
-//   error: string;
-//   success?: undefined;
-// } | {
-//   success: string;
-//   error?: undefined;
-// }>
+
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
@@ -18,22 +12,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   }
 
   const { email, password, name, address, phoneNumber, username } = validatedFields.data;
-  // return await axios.post(process.env.BACKEND_API + "/Authenticate/register", {
-  //   email,
-  //   password,
-  //   name,
-  //   address,
-  //   phoneNumber,
-  //   username
-  // })
-  // .then( (response) => {
-  //     return { success: "Register successfully!" };
-  // })
-  // .catch( (error) => {
-  //   return { error: "Invalid fields!" };
-  // });
   try {
-    // Make a GET request
     const response = await axios.post(process.env.BACKEND_API + "/Authenticate/register", {
       email,
       password,
