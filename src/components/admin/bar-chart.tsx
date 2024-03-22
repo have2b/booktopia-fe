664e507a675cc63c2fb277a45fafde146215b4ed
hopdinhp02 <hopdinhp02@gmail.com>
2024-03-22 23:31:23 +0700

    fix: ignore cannot fix error of recharts in browser console

--- src/components/admin/bar-chart.tsx
+++ src/components/admin/bar-chart.tsx
@@ -10,7 +10,14 @@ import {
   YAxis,
   Bar,
 } from "recharts";
-
+// Override console.error
+// This is a hack to suppress the warning about missing defaultProps in recharts library as of version 2.12
+// @link https://github.com/recharts/recharts/issues/3615
+const error = console.error;
+console.error = (...args: any) => {
+  if (/defaultProps/.test(args[0])) return;
+  error(...args);
+};
 type Props = {};
 
 const revenueData = [
