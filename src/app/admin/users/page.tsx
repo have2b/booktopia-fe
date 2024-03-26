afed5f74ceef22c7a75bed974140f3d30ba2cf6e
hopdinhp02 <hopdinhp02@gmail.com>
2024-03-26 08:59:24 +0700

    feat: users page

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -2,21 +2,18 @@
 
 import { DataTable } from "@/components/admin/data-table";
 import { ColumnDef } from "@tanstack/react-table";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PageTitle from "@/components/admin/page-title";
+import { cn } from "@/lib/utils";
+import { User } from "@/models";
+import { GetUsers } from "@/actions/admin/user";
 
 type Props = {};
-type Payment = {
-  name: string;
-  email: string;
-  lastOrder: string;
-  method: string;
-};
 
-const columns: ColumnDef<Payment>[] = [
+const columns: ColumnDef<User>[] = [
   {
-    accessorKey: "name",
-    header: "Name",
+    accessorKey: "userName",
+    header: "UserName",
     cell: ({ row }) => {
       return (
         <div className="flex gap-2 items-center">
@@ -27,123 +24,56 @@ const columns: ColumnDef<Payment>[] = [
             )}`}
             alt="user-image"
           />
-          <p>{row.getValue("name")} </p>
+          <p>{row.getValue("userName")} </p>
         </div>
       );
     },
+  },
+  {
+    accessorKey: "name",
+    header: "Full Name",
+  },
+  {
+    accessorKey: "phoneNumber",
+    header: "Phone Number",
   },
   {
     accessorKey: "email",
     header: "Email",
   },
   {
-    accessorKey: "lastOrder",
-    header: "Last Order",
-  },
-  {
-    accessorKey: "method",
-    header: "Method",
-  },
-];
-
-const data: Payment[] = [
-  {
-    name: "John Doe",
-    email: "john@example.com",
-    lastOrder: "2023-01-01",
-    method: "Credit Card",
-  },
-  {
-    name: "Alice Smith",
-    email: "alice@example.com",
-    lastOrder: "2023-02-15",
-    method: "PayPal",
-  },
-  {
-    name: "Bob Johnson",
-    email: "bob@example.com",
-    lastOrder: "2023-03-20",
-    method: "Stripe",
-  },
-  {
-    name: "Emma Brown",
-    email: "emma@example.com",
-    lastOrder: "2023-04-10",
-    method: "Venmo",
-  },
-  {
-    name: "Michael Davis",
-    email: "michael@example.com",
-    lastOrder: "2023-05-05",
-    method: "Cash",
-  },
-  {
-    name: "Sophia Wilson",
-    email: "sophia@example.com",
-    lastOrder: "2023-06-18",
-    method: "Bank Transfer",
-  },
-  {
-    name: "Liam Garcia",
-    email: "liam@example.com",
-    lastOrder: "2023-07-22",
-    method: "Payoneer",
-  },
-  {
-    name: "Olivia Martinez",
-    email: "olivia@example.com",
-    lastOrder: "2023-08-30",
-    method: "Apple Pay",
-  },
-  {
-    name: "Noah Rodriguez",
-    email: "noah@example.com",
-    lastOrder: "2023-09-12",
-    method: "Google Pay",
-  },
-  {
-    name: "Ava Lopez",
-    email: "ava@example.com",
-    lastOrder: "2023-10-25",
-    method: "Cryptocurrency",
-  },
-  {
-    name: "Elijah Hernandez",
-    email: "elijah@example.com",
-    lastOrder: "2023-11-05",
-    method: "Alipay",
-  },
-  {
-    name: "Mia Gonzalez",
-    email: "mia@example.com",
-    lastOrder: "2023-12-08",
-    method: "WeChat Pay",
-  },
-  {
-    name: "James Perez",
-    email: "james@example.com",
-    lastOrder: "2024-01-18",
-    method: "Square Cash",
-  },
-  {
-    name: "Charlotte Carter",
-    email: "charlotte@example.com",
-    lastOrder: "2024-02-22",
-    method: "Zelle",
-  },
-  {
-    name: "Benjamin Taylor",
-    email: "benjamin@example.com",
-    lastOrder: "2024-03-30",
-    method: "Stripe",
+    accessorKey: "isActive",
+    cell: ({ row }) => {
+      return (
+        <div
+          className={cn("font-medium w-fit px-4 py-2 rounded-lg", {
+            "bg-red-200": row.getValue("isActive") === false,
+            "bg-green-200": row.getValue("isActive") === true,
+          })}
+        >
+          {row.getValue("isActive") ? "True" : "False"}
+        </div>
+      );
+    },
   },
 ];
 
 export default function UsersPage({}: Props) {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    GetUsers().then((data) => {
+      console.log(data.payload);
+      if (data.payload) {
+        setUsers(data.payload);
+      } else {
+        setUsers([]);
+      }
+    });
+  }, []);
   return (
     <div className="flex flex-col gap-5  w-full">
       <PageTitle title="Users" />
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={users} />
     </div>
   );
 }
